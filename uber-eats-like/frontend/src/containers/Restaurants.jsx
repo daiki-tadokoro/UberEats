35f4959aa79c79e185b1864abce1476a7bdf48cc
田所大樹 <tadokoro@crewinc.io>
2022-03-22 23:33:20 +0900

    addsens

--- uber-eats-like/frontend/src/containers/Restaurants.jsx
+++ uber-eats-like/frontend/src/containers/Restaurants.jsx
@@ -1,11 +1,61 @@
-import React, { Fragment } from "react";
-import { useEffect } from "react";
+import React, { Fragment, useEffect, useReducer } from "react";
+import styled from "styled-components";
+// apis
 import { fetchRestaurants } from "../apis/restaurants";
+
+import {
+  initialState,
+  restaurantsActionTypes,
+  restaurantsReducer,
+} from "../reducers/restaurants";
+
+// images
+import MainLogo from "../images/logo.png";
+import MainCoverImage from "../images/main-cover-image.png";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
   useEffect(() => {
-    fetchRestaurants().then((data) => console.log(data));
+    dispatch({ type: restaurantsActionTypes.FETCHING });
+    fetchRestaurants().then((data) => {
+      dispatch({
+        type: restaurantsActionTypes,
+        FETCH_SUCCES,
+        payload: {
+          restaurants: data.restaurants,
+        },
+      });
+    });
   }, []);
 
-  return <Fragment>レストラン一覧</Fragment>;
+  return (
+    <Fragment>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+      {state.restaurantsList.map((restaurant) => (
+        <div>{restaurant.name}</div>
+      ))}
+    </Fragment>
+  );
 };
